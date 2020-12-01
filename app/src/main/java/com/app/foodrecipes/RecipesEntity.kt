e0900881ed1cb0446d23ef0cf06a14ea0e289bdf
Gilang <initial.gr.101@gmail.com>
2020-12-01 16:16:25 +0700

    part 17 Create Entity (RecipesEntity)

---
+++ app/src/main/java/com/app/foodrecipes/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package com.app.foodrecipes
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.app.foodrecipes.models.FoodRecipe
+import com.app.foodrecipes.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
