c5c35c13039554728a654893d833b8c2d6208b30
Gilang <initial.gr.101@gmail.com>
2020-12-01 10:24:01 +0700

    part 5 Create Constants class & Create RecipesApi interface

---
+++ app/src/main/java/com/app/foodrecipes/Constants.kt
@@ -0,0 +1,10 @@
+package com.app.foodrecipes
+
+class Constants {
+
+    companion object{
+
+        const val BASE_URL ="https://api.spoonacular.com"
+        const val API_KEY = "84e72eef7e914b9da208a56a5ac77465"
+    }
+}
