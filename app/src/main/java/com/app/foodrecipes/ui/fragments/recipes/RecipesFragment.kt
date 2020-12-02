3ad44320843caed95605b2663c07c6635b1f12d0
Gilang <initial.gr.101@gmail.com>
2020-12-02 08:37:44 +0700

    part 28 Navigate to RecipesBottomSheet

--- app/src/main/java/com/app/foodrecipes/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/app/foodrecipes/ui/fragments/recipes/RecipesFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.foodrecipes.viewmodels.MainViewModel
 import com.app.foodrecipes.R
@@ -49,6 +50,10 @@ class RecipesFragment : Fragment() {
 
         setupRecyclerView()
         readDatabase()
+
+        binding.recipesFab.setOnClickListener {
+            findNavController().navigate(R.id.action_recipesFragment_to_recipesBottomSheet)
+        }
 
         return binding.root
     }
